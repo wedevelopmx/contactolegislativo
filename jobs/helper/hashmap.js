bfa00f7d77b3e2ac61b25a733f5bc44c0c585dc1
Cristian Colorado <cristian@wedevelop.mx>
2017-01-18 14:39:06 -0600

    Fix: Error in containsKey function

--- jobs/helper/hashmap.js
+++ jobs/helper/hashmap.js
@@ -4,11 +4,15 @@ var HashMap = function() {
 }
 
 HashMap.prototype.containsKey = function(key) {
-  return this.hash.hasOwnProperty(value);
+  return this.hash.hasOwnProperty(key);
 }
 
 HashMap.prototype.put = function(key, value) {
   this.hash[key] = value;
+}
+
+HashMap.prototype.get = function(key) {
+  return this.hash[key];
 }
 
 module.exports = HashMap;
