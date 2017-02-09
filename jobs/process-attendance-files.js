9a1280b6b5126197ee23074596ebe9848638637f
Cristian Colorado <ccoloradoc@gmail.com>
2017-02-09 15:43:23 -0600

    Integration: Handling processing errors

--- jobs/process-attendance-files.js
+++ jobs/process-attendance-files.js
@@ -7,6 +7,20 @@ var KeyGenerator = require("../jobs/helper/keygenerator");
 
 models.sequelize.sync().then(function () {
 
+    var parseFileDate = function(stringDate) {
+      if(stringDate.length == 8) { //25022016
+        return new Date(stringDate.slice(4,stringDate.length), parseInt(stringDate.slice(2,4)) - 1, stringDate.slice(0,2), 0, 0, 0, 0);
+      } else if(stringDate.length == 6) { //250216
+        return new Date('20' + stringDate.slice(4,stringDate.length), parseInt(stringDate.slice(2,4)) - 1, stringDate.slice(0,2), 0, 0, 0, 0);
+      } else if(stringDate.length == 10) { //25|02|2016
+        date = /(\d+)\|+(\w+)\|+(\d+)/.exec(stringDate);
+        if(date != undefined) {
+          return new Date(date[3], parseInt(date[2]) - 1, date[1], 0, 0, 0, 0);
+        }
+      }
+    }
+
+
     var retriveDate = function(stringDate) {
       //Remove dayname and "de"
       stringDate = stringDate.substr(stringDate.indexOf(',') + 1, stringDate.length).replace(/de/g, '').trim().toLocaleLowerCase();
@@ -20,8 +34,8 @@ models.sequelize.sync().then(function () {
       return new Date(year, month, day, 0, 0, 0, 0);
     }
 
-    var processJSON = function(pdf) {
-      date = '';
+    var processJSON = function(pdf, date) {
+      var fileDate = '';
       attendance = [];
 
       //Iterating Pages
@@ -37,7 +51,7 @@ models.sequelize.sync().then(function () {
             row.push(decodeURI(text.R[0].T));
           } else {
               //End of the row
-
+              //console.log(row.length + ' - ' + row.join(''));
               if(row.length == 3) {
                 last = row[row.length-1];
                 if(last === 'ASISTENCIA'  || last === 'JUSTIFICADA' ||
@@ -50,11 +64,11 @@ models.sequelize.sync().then(function () {
                     name: row[1],
                     hash: namesKeyGen.generateKeyForTerm(row[1], ' '),
                     attendance: row[2],
-                    attendanceDate: date
+                    attendanceDate: fileDate || date
                   });
                 }
-              } else if(date === '' && row.join(' ').lastIndexOf('2016') >= 0) {
-                date = retriveDate(decodeURIComponent(row.join(' ')));
+              } else if(fileDate === '' && row.join(' ').lastIndexOf('2016') >= 0) {
+                fileDate = retriveDate(decodeURIComponent(row.join(' ')));
               }
 
               row = [];
@@ -83,19 +97,28 @@ models.sequelize.sync().then(function () {
 
     var processFiles = function(callback) {
       models.DownloadedFile
-        .findAll({ where: { step: 1 }, limit: 5 }) // Find All downloaded files
+        .findAll({ where: { step: 1 }, limit: 10 }) // Find All downloaded files
         .then(function(files) {
           async.map(files, function(file, callback) {
+            console.log('opening ' + file.name)
             //Convert PDF to JSON
             pdfParser = new PDFParser();
             pdfParser.on("pdfParser_dataError", function(errData) {
               console.log('Error');
+              file.updateAttributes({ step: 999 });
               console.error(errData.parserError)
             });
 
             pdfParser.on("pdfParser_dataReady", function(pdf) {
-                file.updateAttributes({ step: 2 });
-                callback(null, processJSON(pdf));
+                var date = parseFileDate(file.name);
+                var json = processJSON(pdf, date);
+                //fs.writeFile('./downloads/json/' + file.name + '.json', JSON.stringify(pdf) , 'utf-8');
+                console.log('File parsed ' + json.length + ' elements ' + date);
+                if(json.length > 0)
+                  file.updateAttributes({ step: 2 });
+                else
+                  file.updateAttributes({ step: 666 });
+                callback(null, json);
             });
 
             pdfParser.loadPDF("./downloads/pdf/" + file.name + ".pdf");
@@ -106,7 +129,7 @@ models.sequelize.sync().then(function () {
                 bulkAttendance = bulkAttendance.concat(result[i]);
               }
 
-              console.log(bulkAttendance);
+              //console.log(bulkAttendance);
 
               models.AttendanceStg
                 .bulkCreate(bulkAttendance, { ignoreDuplicates: true })
@@ -120,7 +143,7 @@ models.sequelize.sync().then(function () {
                     });
 
                   attendanceStg = attendanceStg.map(function(attn) { return attn.get({ plain: true }); })
-                  console.log(attendanceStg);
+                  //console.log(attendanceStg);
                 });
 
           });
